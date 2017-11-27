7e0cb5da6565f4a8f622656f25b235e6c4d959ff
escamillaj <escamillaj@berea.edu>
2017-11-27 09:03:03 -0500

    Brainstorming and beginning. Made class Draw and class test.

---
+++ Final_Project.py
@@ -0,0 +1,59 @@
+######################################################################
+# Author: Jazmin Escamilla
+# Username: escamillaj
+#
+# modified from http://interactivepython.org/runestone/static/thinkcspy/PythonTurtle/helloturtle.html
+
+# Assignment: Final Project
+# Purpose:
+
+######################################################################
+# Acknowledgements:
+#
+# originally modified by Dr. Jan Pearce
+#
+#
+######################################################################
+
+import turtle
+import random
+
+chance = random.Random()
+wn = turtle.Screen()
+wn.bgcolor(chance)
+
+
+#Creates turtle screen
+#class making
+# find public domain images. Add files.
+
+
+class Test:
+    def __init__(self):
+        """
+        Initializer
+        """
+        
+    def test_questions(self):
+        """
+        
+        :return:
+        """
+        #Game to play: based on how you play it will determine your path.
+        # ethical questions, random questions,
+        #call Draw here
+
+
+
+class Draw:
+    def __init__(self):
+        """
+        Initializer. Sets attributes
+        """
+        #methods- sayings, gif images, squares, triangles,
+        # #turtle.write- list of things choose from
+        # complicated turt images
+        #christmas related
+
+
+#main
