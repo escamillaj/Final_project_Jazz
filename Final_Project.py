64370754ef8b4f2a82f95f933e7b0c87ee9b6df7
escamillaj <escamillaj@berea.edu>
2017-12-10 20:30:12 -0500

    Progress on project. Made frame and canvas with two buttons. Help from TA

--- Final_Project.py
+++ Final_Project.py
@@ -17,23 +17,64 @@
 
 import tkinter as tk
 
+import random
+
 class Art_Show:
+    
     def __init__(self):
         """
         Initializer
         """
-        self.wn = tk()
-        self.canvas = tk.canvas  #.. command= draw_shape)
-  #      self.button = TK.button1
-        self.current_shape= tk.image()
         
-    def draw_shape(self,x,y):
+ #       self.Button1.pack()
+ #       self.current_shape= tk.image()
+
+        self.master = tk.Tk()
+        frame_canvas = tk.Frame(self.master, width=500, height=400)
+        frame_canvas.pack(padx=50, pady=50)
+        self.c = tk.Canvas(frame_canvas, width=1250, height=350, background= "white")
+        self.c.pack()
+        self.Button1 = tk.Button(frame_canvas, command = self.draw_shape)
+        self.Button1.config(text = "Random Shape")
+        self.Button1.pack(padx= 20, pady=30)
+        self.Button2 = tk.Button(frame_canvas, command = self.draw_shape)
+        self.Button2.config(text= "Scenery")
+        self.Button2.pack()
+        
+    
+        
+        
+    def button_handler(self):
+        """
+        
+        :return:
+        """
+        
+    
+    def draw_shape(self):
+        """
+        
+        :param x:
+        :param y:
+        :return:
+        """
         #self.shape= #new shape, random image from self.image
+        circle = tk.PhotoImage(file="Black Circle.gif" )
+        self.c.create_image(2,2,image= circle, anchor='center')
+        
     
     def change_shape(self):
+        """
+        
+        :return:
+        """
         # new shape and random pick from self.images
 
-def main():
-    a = Art_Show()
-    
+
+if __name__ == "__main__":
+    a = Art_Show()      # creates a new tkinter object
+    a.c.mainloop()
+  
+    main()
+
 # What is new, tk.image   and tk canvas
